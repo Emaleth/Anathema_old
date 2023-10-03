518028530a13dae01a0e8c394bd686c37d4595e1
Emaleth <emaleth@protonmail.com>
2023-10-03 23:07:31 +0200

    Changes to be committed: 	new file:   assets/sounds/footsteps/footstep00.ogg 	new file:   assets/sounds/footsteps/footstep00.ogg.import 	new file:   assets/sounds/footsteps/footstep01.ogg 	new file:   assets/sounds/footsteps/footstep01.ogg.import 	new file:   assets/sounds/footsteps/footstep02.ogg 	new file:   assets/sounds/footsteps/footstep02.ogg.import 	new file:   assets/sounds/footsteps/footstep03.ogg 	new file:   assets/sounds/footsteps/footstep03.ogg.import 	new file:   assets/sounds/footsteps/footstep04.ogg 	new file:   assets/sounds/footsteps/footstep04.ogg.import 	new file:   assets/sounds/footsteps/footstep05.ogg 	new file:   assets/sounds/footsteps/footstep05.ogg.import 	new file:   assets/sounds/footsteps/footstep06.ogg 	new file:   assets/sounds/footsteps/footstep06.ogg.import 	new file:   assets/sounds/footsteps/footstep07.ogg 	new file:   assets/sounds/footsteps/footstep07.ogg.import 	new file:   assets/sounds/footsteps/footstep08.ogg 	new file:   assets/sounds/footsteps/footstep08.ogg.import 	new file:   assets/sounds/footsteps/footstep09.ogg 	new file:   assets/sounds/footsteps/footstep09.ogg.import 	modified:   source/actors/player/player.gd 	modified:   source/actors/player/player.tscn 	modified:   world.tscn

--- source/actors/player/player.gd
+++ source/actors/player/player.gd
@@ -8,6 +8,23 @@ const JUMP_VELOCITY = 4.5
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 var mouse_sensitivity := 0.01
 @onready var camera := $Camera3D
+@onready var footsteps_audio := $FootstepsAudio
+
+
+var travelled_distance := 0
+var step_lenght := 2.0
+var footstep_sounds := [
+	preload("res://assets/sounds/footsteps/footstep00.ogg"),
+	preload("res://assets/sounds/footsteps/footstep01.ogg"),
+	preload("res://assets/sounds/footsteps/footstep02.ogg"),
+	preload("res://assets/sounds/footsteps/footstep06.ogg"),
+	preload("res://assets/sounds/footsteps/footstep05.ogg"),
+	preload("res://assets/sounds/footsteps/footstep04.ogg"),
+	preload("res://assets/sounds/footsteps/footstep03.ogg"),
+	preload("res://assets/sounds/footsteps/footstep08.ogg"),
+	preload("res://assets/sounds/footsteps/footstep09.ogg"),
+	preload("res://assets/sounds/footsteps/footstep07.ogg"),
+]
 
 func _ready():
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
@@ -28,6 +45,7 @@ func _physics_process(delta):
 	if direction:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
+		footsteps()
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
@@ -40,3 +58,11 @@ func _input(event):
 		rotation_degrees.y = wrap(rotation_degrees.y, -180, 180)
 		camera.rotate_x(event.relative.y * mouse_sensitivity * -1)
 		camera.rotation_degrees.x = clamp(camera.rotation_degrees.x, -90, 90)
+
+func footsteps():
+	if is_on_floor():
+		travelled_distance += velocity.length()
+		if travelled_distance >= step_lenght:
+			travelled_distance = 0.0
+			footsteps_audio.stream = footstep_sounds.pick_random()
+			footsteps_audio.play()
