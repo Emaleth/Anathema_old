de6c6d39db0ac21d560c7a950bcf24def01c1b6b
Emaleth <emaleth@protonmail.com>
2023-11-05 09:49:31 +0100

    Changes to be committed: 	new file:   source/actors/player/old_player.gd 	modified:   source/actors/player/player.gd 	modified:   source/actors/player/player.tscn

--- source/actors/player/player.gd
+++ source/actors/player/player.gd
@@ -1,9 +1,12 @@
 extends CharacterBody3D
-# FIXME: SPRINTING SPEED WHEN ADSING
+
+enum {IDLE, RUN, JUMP, FALL, SPRINT, CROUCH}
+enum {HIPFIRE, ADS}
 
 const RUNNING_SPEED = 5.0
 const CROUCHING_SPEED = 2.0
 const SPRINTING_SPEED = 10.0
+const SLIDING_SPEED = 10.0
 const JUMP_VELOCITY = 4.5
 const STANDING_HEAD_HEIGHT := 0.75
 const CROUCHING_HEAD_HEIGHT := 0.2
@@ -13,7 +16,7 @@ const JUMP_GRACE_PERIOD := 1.0
 const STEP_LENGHT := 1.5
 
 var ads_stance := Vector3(0.0, 0.0, -0.2)
-var normal_stance := Vector3(0.4, -0.1, -0.6)
+var hipfire_stance := Vector3(0.4, -0.1, -0.6)
 var acceleraion := 0.9
 var normal_deceleraion := 0.9
 var slide_deceleraion := 0.001
@@ -36,12 +39,14 @@ var jumped := false
 @onready var standing_collision_shape := $StandingCollisionShape
 @onready var crouching_collision_shape := $CrouchingCollisionShape
 @onready var head_raycast := $RayCast3D2
+@onready var chest := $UpperBody/Head/Chest
 @onready var feet_raycast := $RayCast3D
 @onready var viewport_size : Vector2 = get_viewport().size
 @onready var right_hand := $UpperBody/Head/Chest/RightHand
 @onready var left_hand := $UpperBody/Head/Chest/LeftHand
 @onready var upper_body := $UpperBody
 @onready var voice_audio := $UpperBody/Head/VoiceAudio
+@onready var fps_arms := $UpperBody/Head/Chest/FPSArms
 
 var sin_time := 0.0
 var sin_frequency := 2.0
@@ -50,22 +55,35 @@ var last_in_air_velocity := 0.0
 var mouse_motion_event_relative_x := 0.0
 var mouse_motion_event_relative_y := 0.0
 var direction := Vector3.ZERO
-@export var crouched := false # just for the crouch / stand animation
 var tilt := 0.0
-var sprinting := false
 var adsing := false
 var weapon : Node3D
-@onready var fps_arms := $UpperBody/Head/Chest/FPSArms
-
-
-signal sig_ads
-signal sig_no_ads
+var motion_state : int
+var aim_state : int
+var hand_position := Vector3.ZERO
+var hand_tilt := Vector3.ZERO
 
 
 func _ready():
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 	weapon = right_hand.get_child(0) # tmp
 	arms_ik_setup()
+	switch_motion_state(IDLE)
+	switch_aim_state(HIPFIRE)
+
+
+func _physics_process(delta: float) -> void:
+	hand_tilt = Vector3.ZERO
+	get_direction()
+	get_tilt()
+	motion_fsm(delta)
+	aim_fsm(delta)
+	rotate_camera()
+	rotate_player()
+	hand_stance()
+	weapon_sway()
+	arm_swing(delta)
+	reset_mouse_motion_event_relative()
 
 
 func arms_ik_setup():
@@ -75,90 +93,109 @@ func arms_ik_setup():
 	fps_arms.left_arm_ik.start()
 
 
-func _physics_process(delta):
-	get_direction()
-	get_tilt()
-	check_is_on_floor(delta)
-	rotate_camera()
-	arm_swing(delta)
-	rotate_player()
-	ads()
-	tilt_head()
-	tilt_upper_body()
-	weapon_sway()
-	footsteps()
-	reset_mouse_motion_event_relative()
-
-	if not is_on_floor():
-		velocity.y -= gravity * delta
-
-	if Input.is_action_pressed("crouch") and is_on_floor():
-		crouching = true
-		if not crouched: animation_player.play("crouch")
+func switch_motion_state(_new_state : int):
+	if _new_state == motion_state:
+		return
 	else:
-		if crouched: animation_player.play("stand")
-		crouching = false
+		motion_state = _new_state
 
-	if Input.is_action_just_pressed("reload"):
-		weapon.reload()
-	if Input.is_action_pressed("primary_action"):
-		weapon.use()
-#		recoil_force += Vector2(0.01, 0.01 * sign(randf()*2-1))
-	if Input.is_action_pressed("secondary_action"):
-		adsing = true
-		sig_ads.emit()
+
+func switch_aim_state(_new_state : int):
+	if _new_state == aim_state:
+		return
 	else:
-		adsing = false
-		sig_no_ads.emit()
+		aim_state = _new_state
 
-	if crouching or sliding:
-		current_speed = CROUCHING_SPEED
-		head.position.y = lerp(head.position.y, CROUCHING_HEAD_HEIGHT, 0.3)
-		standing_collision_shape.set_deferred("disabled", true)
-		crouching_collision_shape.set_deferred("disabled", false)
-	else:
-		if not head_raycast.is_colliding():
-			head.position.y = lerp(head.position.y, STANDING_HEAD_HEIGHT, 0.3)
-			standing_collision_shape.set_deferred("disabled", false)
-			crouching_collision_shape.set_deferred("disabled", true)
-			if Input.is_action_pressed("sprint") and grounded:
-				current_speed = SPRINTING_SPEED
-				sprinting = true
-			else:
-				current_speed = RUNNING_SPEED
-				sprinting = false
 
-	if Input.is_action_pressed("jump") and grounded and not jumped:
-		animation_player.play("jump")
-		velocity.y = JUMP_VELOCITY
-		voice_audio.play()
-		jumped = true
-	if not is_on_floor() and velocity.y < 0:
-		last_in_air_velocity = velocity.y
-	if is_on_floor() and last_in_air_velocity < 0:
-		animation_player.play("land")
-		jumped = false
-		last_in_air_velocity = 0
-	if sliding:
-		current_speed = SPRINTING_SPEED
-		slide_timer += delta
-		if slide_timer > slide_max_time:
-			sliding = false
-			slide_timer = 0.0
-		velocity.x = move_toward(velocity.x, 0, slide_deceleraion)
-		velocity.z = move_toward(velocity.z, 0, slide_deceleraion)
-	elif direction:
-		velocity.x = move_toward(velocity.x, direction.x * current_speed, acceleraion)
-		velocity.z = move_toward(velocity.z, direction.z * current_speed, acceleraion)
-	else:
-		if crouching and sprinting:
-			sliding = true
-		else:
-			velocity.x = move_toward(velocity.x, 0, normal_deceleraion)
-			velocity.z = move_toward(velocity.z, 0, normal_deceleraion)
+func motion_fsm(delta):
+	match motion_state:
+		IDLE:
+			hand_position = Vector3.ZERO
+		RUN:
+			hand_position = Vector3.ZERO
+		JUMP:
+			hand_position = Vector3.ZERO
+		FALL:
+			hand_position = Vector3.ZERO
+		SPRINT:
+			hand_position = Vector3(65.0, 15.0, 15.0)
+		CROUCH:
+			hand_position = Vector3.ZERO
 
-	move_and_slide()
 
+func aim_fsm(delta):
+	match aim_state:
+		HIPFIRE:
+			hipfire_mode()
+			sin_frequency = lerp(sin_frequency, 2.0, 0.2)
+			sin_amplitude = lerp(sin_amplitude, 0.0003, 0.2)
+			get_hand_tilt()
+		ADS:
+			if motion_state == SPRINT:
+				switch_motion_state(RUN)
+			ads_mode()
+			sin_frequency = lerp(sin_frequency, 0.0, 0.2)
+			sin_amplitude = lerp(sin_amplitude, 0.0000, 0.2)
+
+
+func arm_swing(delta):
+	chest.position.y += cos(sin_time * sin_frequency) * sin_amplitude
+	chest.position.x += sin(sin_time * sin_frequency * 0.5) * sin_amplitude
+	sin_time += delta
+
+
+func rotate_camera():
+	head.rotation.x += mouse_motion_event_relative_y * Settings.mouse_sensitivity * -1
+	head.rotation_degrees.x = clamp(head.rotation_degrees.x, -90, 90)
+
+
+func rotate_player():
+	rotation.y += mouse_motion_event_relative_x * Settings.mouse_sensitivity * -1
+	rotation_degrees.y = wrap(rotation_degrees.y, -180, 180)
+
+
+func ads_mode():
+	weapon.set_ads(true)
+	right_hand.position = lerp(right_hand.position, ads_stance, 0.3)
+
+
+func hipfire_mode():
+	weapon.set_ads(false)
+	right_hand.position = lerp(right_hand.position, hipfire_stance, 0.3)
+
+
+func hand_stance():
+	right_hand.rotation_degrees.x = lerp(right_hand.rotation_degrees.x, hand_position.x, 0.1)
+	right_hand.rotation_degrees.y = lerp(right_hand.rotation_degrees.y, hand_position.y, 0.1)
+	right_hand.rotation_degrees.z = lerp(right_hand.rotation_degrees.z, hand_position.z, 0.3)
+
+
+func get_hand_tilt():
+	if abs(mouse_motion_event_relative_y / viewport_size.y) > hand_tilt_deadzone:
+		hand_tilt.x = sign(mouse_motion_event_relative_y)
+	if abs(mouse_motion_event_relative_x / viewport_size.x) > hand_tilt_deadzone:
+		hand_tilt.y = sign(mouse_motion_event_relative_x)
+		hand_tilt.z = sign(mouse_motion_event_relative_x)
+
+
+func weapon_sway():
+	right_hand.rotation_degrees.x += lerp(right_hand.rotation_degrees.x, sign(hand_tilt.x) * weapon_sway_amount, 0.1)
+	right_hand.rotation_degrees.y += lerp(right_hand.rotation_degrees.y, sign(hand_tilt.y) * weapon_sway_amount, 0.1)
+	right_hand.rotation_degrees.z += lerp(right_hand.rotation_degrees.z, sign(hand_tilt.z) * weapon_sway_amount, 0.3)
+
+
+func _on_right_hand_child_entered_tree(node: Node) -> void:
+	set_visibility(node)
+
+
+func _on_left_hand_child_entered_tree(node: Node) -> void:
+	set_visibility(node)
+
+
+func set_visibility(node : Node):
+	for i in node.get_children():
+		if i is MeshInstance3D or i is GPUParticles3D:
+			i.layers = 2
 
 func _input(event):
 	if event is InputEventMouseMotion:
@@ -181,134 +218,140 @@ func get_tilt():
 func reset_mouse_motion_event_relative():
 		mouse_motion_event_relative_x = 0
 		mouse_motion_event_relative_y = 0
+#
+####################################################################################################
+####################################################################################################
+#####################################################################################################
+#func _physics_process(delta):
+#	check_is_on_floor(delta)
+#	tilt_head()
+#	tilt_upper_body()
+#	weapon_sway()
+#	footsteps()
 
+#
+#	if not is_on_floor():
+#		velocity.y -= gravity * delta
+#
+#	if Input.is_action_pressed("crouch") and is_on_floor():
+#		crouching = true
+#		if not crouched: animation_player.play("crouch")
+#	else:
+#		if crouched: animation_player.play("stand")
+#		crouching = false
+#
+#	if Input.is_action_just_pressed("reload"):
+#		weapon.reload()
+#	if Input.is_action_pressed("primary_action"):
+#		weapon.use()
+##		recoil_force += Vector2(0.01, 0.01 * sign(randf()*2-1))
+#	if Input.is_action_pressed("secondary_action"):
+#		adsing = true
+#		sig_ads.emit()
+#	else:
+#		adsing = false
+#		sig_no_ads.emit()
+#
+#	if crouching or sliding:
+#		current_speed = CROUCHING_SPEED
+#		head.position.y = lerp(head.position.y, CROUCHING_HEAD_HEIGHT, 0.3)
+#		standing_collision_shape.set_deferred("disabled", true)
+#		crouching_collision_shape.set_deferred("disabled", false)
+#	else:
+#		if not head_raycast.is_colliding():
+#			head.position.y = lerp(head.position.y, STANDING_HEAD_HEIGHT, 0.3)
+#			standing_collision_shape.set_deferred("disabled", false)
+#			crouching_collision_shape.set_deferred("disabled", true)
+#			if Input.is_action_pressed("sprint") and grounded:
+#				current_speed = SPRINTING_SPEED
+#				sprinting = true
+#			else:
+#				current_speed = RUNNING_SPEED
+#				sprinting = false
+#
+#	if Input.is_action_pressed("jump") and grounded and not jumped:
+#		animation_player.play("jump")
+#		velocity.y = JUMP_VELOCITY
+#		voice_audio.play()
+#		jumped = true
+#	if not is_on_floor() and velocity.y < 0:
+#		last_in_air_velocity = velocity.y
+#	if is_on_floor() and last_in_air_velocity < 0:
+#		animation_player.play("land")
+#		jumped = false
+#		last_in_air_velocity = 0
+#	if sliding:
+#		current_speed = SPRINTING_SPEED
+#		slide_timer += delta
+#		if slide_timer > slide_max_time:
+#			sliding = false
+#			slide_timer = 0.0
+#		velocity.x = move_toward(velocity.x, 0, slide_deceleraion)
+#		velocity.z = move_toward(velocity.z, 0, slide_deceleraion)
+#	elif direction:
+#		velocity.x = move_toward(velocity.x, direction.x * current_speed, acceleraion)
+#		velocity.z = move_toward(velocity.z, direction.z * current_speed, acceleraion)
+#	else:
+#		if crouching and sprinting:
+#			sliding = true
+#		else:
+#			velocity.x = move_toward(velocity.x, 0, normal_deceleraion)
+#			velocity.z = move_toward(velocity.z, 0, normal_deceleraion)
+#
+#	move_and_slide()
+#
+#
+#
+#func check_is_on_floor(delta):
+#	grounded = false
+#	if is_on_floor():
+#		grounded = true
+#	elif feet_raycast.is_colliding():
+#		grounded = true
+#	else:
+#		time_since_grounded += delta
+#		if time_since_grounded <= 0.1:
+#			grounded = true
+#			time_since_grounded = 0
+#
+#
+#func footsteps():
+#	if is_on_floor():
+#		var current_position = Vector2(position.x, position.z)
+#		if current_position.distance_squared_to(last_step_position) > pow(STEP_LENGHT, 2):
+#			footsteps_audio.pitch_scale = randf_range(0.9, 1.1)
+#			footsteps_audio.play()
+#			last_step_position = Vector2(position.x, position.z)
+#
+#
+#
+#
+#
+#
+#func tilt_head():
+#	var head_tilt := 0.0
+#	if abs(mouse_motion_event_relative_x / viewport_size.x) > head_tilt_deadzone:
+#		head_tilt = -mouse_motion_event_relative_x
+#	head.rotation_degrees.z = lerp(head.rotation_degrees.z, HEAD_TILT_DEGREES * sign(head_tilt), 0.1)
+#
+#
+#func tilt_upper_body():
+#	upper_body.rotation_degrees.z = lerp(upper_body.rotation_degrees.z, UPPER_BODY_TILT_DEGREES * tilt, 0.1)
+#
+#
+#
+#
+#
+#
 
-func check_is_on_floor(delta):
-	grounded = false
-	if is_on_floor():
-		grounded = true
-	elif feet_raycast.is_colliding():
-		grounded = true
-	else:
-		time_since_grounded += delta
-		if time_since_grounded <= 0.1:
-			grounded = true
-			time_since_grounded = 0
-
-
-func footsteps():
-	if is_on_floor():
-		var current_position = Vector2(position.x, position.z)
-		if current_position.distance_squared_to(last_step_position) > pow(STEP_LENGHT, 2):
-			footsteps_audio.pitch_scale = randf_range(0.9, 1.1)
-			footsteps_audio.play()
-			last_step_position = Vector2(position.x, position.z)
-
-
-func rotate_camera():
-	head.rotation.x += mouse_motion_event_relative_y * Settings.mouse_sensitivity * -1
-	head.rotation_degrees.x = clamp(head.rotation_degrees.x, -90, 90)
-
-
-func weapon_sway():
-	var hand_tilt_x := 0.0
-	var hand_tilt_y := 0.0
-	var hand_tilt_z := 0.0
-	var hand_position := Vector3.ZERO
-
-	if not adsing:
-		if abs(mouse_motion_event_relative_y / viewport_size.y) > hand_tilt_deadzone:
-			hand_tilt_x = mouse_motion_event_relative_y
-		if abs(mouse_motion_event_relative_x / viewport_size.x) > hand_tilt_deadzone:
-			hand_tilt_y = mouse_motion_event_relative_x
-			hand_tilt_z = mouse_motion_event_relative_x
-
-	if sprinting:
-		hand_position.x = 65.0
-		hand_position.y = 15.0
-		hand_position.y = 15.0
-	else:
-		hand_position.x = 0.0
-		hand_position.y = 0.0
-		hand_position.y = 0.0
-	# RIGHT
-	right_hand.rotation_degrees.x = lerp(right_hand.rotation_degrees.x, sign(hand_tilt_x) * weapon_sway_amount + hand_position.x, 0.1)
-	right_hand.rotation_degrees.y = lerp(right_hand.rotation_degrees.y, sign(hand_tilt_y) * weapon_sway_amount + hand_position.y, 0.1)
-	right_hand.rotation_degrees.z = lerp(right_hand.rotation_degrees.z, sign(hand_tilt_z) * weapon_sway_amount + hand_position.z, 0.3)
-	# LEFT
-	if not adsing:
-		left_hand.rotation_degrees.x = lerp(left_hand.rotation_degrees.x, sign(hand_tilt_x) * weapon_sway_amount + hand_position.x, 0.1)
-		left_hand.rotation_degrees.y = lerp(left_hand.rotation_degrees.y, sign(hand_tilt_y) * weapon_sway_amount + -hand_position.y, 0.1)
-		left_hand.rotation_degrees.z = lerp(left_hand.rotation_degrees.z, sign(hand_tilt_z) * weapon_sway_amount + -hand_position.z, 0.3)
-
-
-func rotate_player():
-	rotation.y += mouse_motion_event_relative_x * Settings.mouse_sensitivity * -1
-	rotation_degrees.y = wrap(rotation_degrees.y, -180, 180)
-
-
-func tilt_head():
-	var head_tilt := 0.0
-	if abs(mouse_motion_event_relative_x / viewport_size.x) > head_tilt_deadzone:
-		head_tilt = -mouse_motion_event_relative_x
-	head.rotation_degrees.z = lerp(head.rotation_degrees.z, HEAD_TILT_DEGREES * sign(head_tilt), 0.1)
-
-
-func tilt_upper_body():
-	upper_body.rotation_degrees.z = lerp(upper_body.rotation_degrees.z, UPPER_BODY_TILT_DEGREES * tilt, 0.1)
-
-func ads():
-	if adsing:
-		weapon.set_ads(true)
-		sprinting = false
-		right_hand.position = lerp(right_hand.position, ads_stance, 0.3)
-#		left_hand.position = lerp(left_hand.position, ads_stance * Vector3(-1.0, 1.0, 1.0), 0.3)
-		# LEFT
-		var hand_position := Vector3.ZERO
-		hand_position.x = 65.0
-		hand_position.y = 15.0
-		hand_position.y = 15.0
-		left_hand.rotation_degrees.x = lerp(left_hand.rotation_degrees.x, hand_position.x, 0.1)
-		left_hand.rotation_degrees.y = lerp(left_hand.rotation_degrees.y, -hand_position.y, 0.1)
-		left_hand.rotation_degrees.z = lerp(left_hand.rotation_degrees.z, -hand_position.z, 0.3)
-		left_hand.position = lerp(left_hand.position, normal_stance * Vector3(-1.0, 1.0, 1.0), 0.3)
-	else:
-		weapon.set_ads(false)
-		right_hand.position = lerp(right_hand.position, normal_stance, 0.3)
-		left_hand.position = lerp(left_hand.position, normal_stance * Vector3(-1.0, 1.0, 1.0), 0.3)
-
-
-@onready var chest := $UpperBody/Head/Chest
-func arm_swing(delta):
-	if not adsing:
-		chest.position.y += cos(sin_time * sin_frequency) * sin_amplitude
-		chest.position.x += sin(sin_time * sin_frequency * 0.5) * sin_amplitude
-	else:
-		chest.position = Vector3.ZERO
-
-	sin_time += delta
-
-
-func _on_right_hand_child_entered_tree(node: Node) -> void:
-	set_visibility(node)
-
-
-func _on_left_hand_child_entered_tree(node: Node) -> void:
-	set_visibility(node)
-
-
-func set_visibility(node : Node):
-	for i in node.get_children():
-		if i is MeshInstance3D or i is GPUParticles3D:
-			i.layers = 2
-
-
-func recoil(recoil_force : Vector2):
-	rotation.y += recoil_force.y
-	head.rotation.x += recoil_force.x
-	rotation_degrees.y = wrap(rotation_degrees.y, -180, 180)
-	head.rotation_degrees.x = clamp(head.rotation_degrees.x, -90, 90)
-
-var sliding := false
-var slide_timer := 0.0
-var slide_max_time := 3.0
+#
+#
+#func recoil(recoil_force : Vector2):
+#	rotation.y += recoil_force.y
+#	head.rotation.x += recoil_force.x
+#	rotation_degrees.y = wrap(rotation_degrees.y, -180, 180)
+#	head.rotation_degrees.x = clamp(head.rotation_degrees.x, -90, 90)
+#
+#var sliding := false
+#var slide_timer := 0.0
+#var slide_max_time := 3.0
