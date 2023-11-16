d19655223fefbb6811c6f0067b98b32cedd38b38
Emaleth <emaleth@protonmail.com>
2023-11-16 16:38:33 +0100

    Changes to be committed: 	deleted:    assets/crosshairs/crosshair001.png 	deleted:    assets/crosshairs/crosshair001.png.import 	deleted:    assets/crosshairs/crosshair002.png 	deleted:    assets/crosshairs/crosshair002.png.import 	deleted:    assets/crosshairs/crosshair003.png 	deleted:    assets/crosshairs/crosshair003.png.import 	modified:   source/actors/player/player.tscn 	modified:   source/user_interface/user_interface.gd 	modified:   source/user_interface/user_interface.tscn 	modified:   source/weapons/blaster_d.tscn

--- source/user_interface/user_interface.gd
+++ source/user_interface/user_interface.gd
@@ -1,6 +1,5 @@
 extends Control
 
-@onready var crosshair := $MarginContainer/CenterContainer/Crosshair
 @onready var current_ammo_label := $MarginContainer/Ammo/HBoxContainer/Current
 @onready var max_ammo_label := $MarginContainer/Ammo/HBoxContainer/Max
 
@@ -11,8 +10,28 @@ func _ready() -> void:
 	Signals.update_max_ammo.connect(update_max_ammo)
 
 
+func _draw() -> void:
+	var screen_center := size * 0.5
+	var start_offset := 5
+	var end_offset := 15
+	var crosshair_color := Color.WHITE
+	var line_size := 0.5
+	if crosshair:
+#		crosshair_color = Color.WHITE
+#	else:
+#		crosshair_color = Color.TRANSPARENT
+		draw_line(screen_center + Vector2(start_offset, 0), screen_center + Vector2(end_offset, 0), crosshair_color, line_size, true)
+		draw_line(screen_center + Vector2(-start_offset, 0), screen_center + Vector2(-end_offset, 0), crosshair_color, line_size, true)
+		draw_line(screen_center + Vector2(0, start_offset), screen_center + Vector2(0, end_offset), crosshair_color, line_size, true)
+		draw_line(screen_center + Vector2(0, -start_offset), screen_center + Vector2(0, -end_offset), crosshair_color, line_size, true)
+		queue_redraw()
+
+
+
+
+var crosshair = true
 func show_crosshair(value : bool):
-	crosshair.visible = not value
+	crosshair = value
 
 
 func update_max_ammo(value : int):
