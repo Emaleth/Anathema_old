77711c2f2453e4d6949f7143df91ea8f814d05e4
Emaleth <emaleth@protonmail.com>
2023-12-04 08:34:49 +0100

    Changes to be committed: 	new file:   source/actors/player/Animations.gd 	modified:   source/actors/player/player.gd 	modified:   source/actors/player/player.tscn 	deleted:    source/qodot_map.tscn

---
+++ source/actors/player/Animations.gd
@@ -0,0 +1,88 @@
+extends Node
+
+const HEAD_TILT_DEGREES := 25.0
+const HIPFIRE_SIN_FREQUENCY := 2.0
+const HIPFIRE_SIN_AMPLITUDE := 0.03
+const ADS_SIN_FREQUENCY := 1.0
+const ADS_SIN_AMPLITUDE := 0.001
+
+var head_tilt_deadzone := 0.05
+var hand_tilt_deadzone := 0.001
+var mouse_motion_event_relative := Vector2.ZERO
+var sin_time := 0.0
+var sin_frequency := 0.0
+var sin_amplitude := 0.0
+var weapon_sway_amount := 3.0
+
+@onready var viewport_size : Vector2 = get_viewport().size
+
+
+func _ready() -> void:
+	get_tree().process_frame.connect(reset_mouse_motion_event_relative)
+
+
+func _input(event):
+	if event is InputEventMouseMotion:
+		mouse_motion_event_relative.x = event.relative.x
+		mouse_motion_event_relative.y = event.relative.y
+
+
+func jump_animation(camera : Camera3D):
+	var tween = create_tween()
+	tween.tween_property( camera, "position:y", 0.1, 0.1 ).set_trans(Tween.TRANS_LINEAR)
+	tween.parallel().tween_property( camera, "rotation:x", deg_to_rad(-5), 0.1 ).set_trans(Tween.TRANS_LINEAR)
+
+	tween.tween_property( camera, "position:y", 0.0, 0.2 ).set_trans(Tween.TRANS_LINEAR)
+	tween.parallel().tween_property( camera, "rotation:x", 0.0, 0.2 ).set_trans(Tween.TRANS_LINEAR)
+
+
+func land_animation(camera : Camera3D):
+	var tween = create_tween()
+	tween.tween_property( camera, "position:y", 0.1, 0.1 ).set_trans(Tween.TRANS_LINEAR)
+	tween.parallel().tween_property( camera, "rotation:x", deg_to_rad(-5), 0.1 ).set_trans(Tween.TRANS_LINEAR)
+
+	tween.tween_property( camera, "position:y", 0.0, 0.2 ).set_trans(Tween.TRANS_LINEAR)
+	tween.parallel().tween_property( camera, "rotation:x", 0.0, 0.2 ).set_trans(Tween.TRANS_LINEAR)
+
+
+func head_bob():
+	pass
+
+
+func tilt_head(head : Node3D):
+	var head_tilt := 0.0
+	if abs(mouse_motion_event_relative.x / viewport_size.x) > head_tilt_deadzone:
+		head_tilt = -mouse_motion_event_relative.x
+	head.rotation_degrees.z = lerp(head.rotation_degrees.z, HEAD_TILT_DEGREES * sign(head_tilt), 0.1)
+
+
+func arm_swing(chest : Node3D, aim_state : int, delta : float):
+	match aim_state:
+		0:
+			sin_amplitude = HIPFIRE_SIN_AMPLITUDE
+			sin_frequency = HIPFIRE_SIN_FREQUENCY
+		1:
+			sin_amplitude = ADS_SIN_AMPLITUDE
+			sin_frequency = ADS_SIN_FREQUENCY
+	chest.position.y = cos(sin_time * sin_frequency) * sin_amplitude
+	chest.position.x = sin(sin_time * sin_frequency * 0.5) * sin_amplitude
+	sin_time += delta
+
+
+func weapon_sway(right_weapon_pivot : Node3D, aim_state : int):
+	var hand_tilt := Vector3.ZERO
+	if aim_state == 0:
+		if abs(mouse_motion_event_relative.y / viewport_size.y) > hand_tilt_deadzone:
+			hand_tilt.x = sign(mouse_motion_event_relative.y)
+		if abs(mouse_motion_event_relative.x / viewport_size.x) > hand_tilt_deadzone:
+			hand_tilt.y = sign(mouse_motion_event_relative.x)
+			hand_tilt.z = sign(mouse_motion_event_relative.x)
+
+	right_weapon_pivot.rotation_degrees.x = lerp(right_weapon_pivot.rotation_degrees.x, sign(hand_tilt.x) * weapon_sway_amount, 0.1)
+	right_weapon_pivot.rotation_degrees.y = lerp(right_weapon_pivot.rotation_degrees.y, sign(-hand_tilt.y) * weapon_sway_amount, 0.1)
+	right_weapon_pivot.rotation_degrees.z = lerp(right_weapon_pivot.rotation_degrees.z, sign(hand_tilt.z) * weapon_sway_amount, 0.1)
+
+
+func reset_mouse_motion_event_relative():
+		mouse_motion_event_relative.x = 0
+		mouse_motion_event_relative.y = 0
--- source/actors/player/player.gd
+++ source/actors/player/player.gd
@@ -8,32 +8,23 @@ const aim_states_array := ["HIPFIRE", "ADS"]
 const RUNNING_SPEED = 5.0
 const CROUCHING_SPEED = 2.0
 const SPRINTING_SPEED = 10.0
-const SLIDING_SPEED = 10.0
+const SLIDING_SPEED = 15.0
+const AIR_SPEED = 5.0
 const JUMP_FORCE = 5.0
 const STANDING_HEAD_HEIGHT := 0.75
 const CROUCHING_HEAD_HEIGHT := 0.2
 const SLIDING_HEAD_HEIGHT := 0.0
-const HEAD_TILT_DEGREES := 25.0
 const UPPER_BODY_TILT_DEGREES := 27.0
 const STEP_LENGHT := 1.5
-const HIPFIRE_SIN_FREQUENCY := 2.0
-const HIPFIRE_SIN_AMPLITUDE := 0.03
-const ADS_SIN_FREQUENCY := 1.0
-const ADS_SIN_AMPLITUDE := 0.001
 const ADS_STANCE := Vector3(0.0, 0.0, -0.2)
 const HIPFIRE_STANCE := Vector3(0.25, -0.4, -0.6)
 
 var motion_state_entered := false
 var aim_state_entered := false
 var acceleraion := 0.9
-var normal_deceleraion := 0.9
-var slide_deceleraion := 0.001
-var head_tilt_deadzone := 0.05
-var hand_tilt_deadzone := 0.001
 var current_speed := 0.0
 var gravity : float = ProjectSettings.get_setting("physics/3d/default_gravity")
 var last_step_position := Vector2.ZERO
-var weapon_sway_amount := 3.0
 var mouse_sensitivity := 0.0
 var ads_position_offset := Vector3.ZERO
 
@@ -47,7 +38,6 @@ var ads_position_offset := Vector3.ZERO
 @onready var crouching_collision_shape := $CrouchingCollisionShape
 @onready var head_raycast := $RayCast3D2
 @onready var chest := $UpperBody/Head/Chest
-@onready var feet_raycast := $RayCast3D
 @onready var viewport_size : Vector2 = get_viewport().size
 @onready var right_hand := $UpperBody/Head/Chest/RightHand
 @onready var right_weapon_pivot := $UpperBody/Head/Chest/RightHand/WeaponPivot
@@ -56,24 +46,20 @@ var ads_position_offset := Vector3.ZERO
 @onready var voice_audio := $UpperBody/Head/VoiceAudio
 @onready var fps_arms := $UpperBody/Head/Chest/FPSArms
 @onready var camera_ray := $UpperBody/Head/RayCast3D
+@onready var animations := $Animations
 
-var sin_time := 0.0
-var sin_frequency := 0.0
-var sin_amplitude := 0.0
-var last_in_air_velocity := 0.0
-var mouse_motion_event_relative_x := 0.0
-var mouse_motion_event_relative_y := 0.0
+var mouse_motion_event_relative := Vector2.ZERO
 var direction := Vector3.ZERO
 var tilt := 0.0
 var motion_state : int
 var aim_state : int
 var hand_position := Vector3.ZERO
-var hand_tilt := Vector3.ZERO
 var slide_start_position := Vector3.ZERO
 var slide_max_distance := 10
 
 
 func _ready():
+	get_tree().process_frame.connect(reset_mouse_motion_event_relative)
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 	arms_ik_setup()
 	switch_motion_state(IDLE)
@@ -90,18 +76,17 @@ func _process(_delta: float) -> void:
 
 
 func _physics_process(delta: float) -> void:
-	hand_tilt = Vector3.ZERO
 	get_direction()
 	get_tilt()
-	tilt_head()
-	head_bob()
+	animations.tilt_head(head)
+	animations.head_bob()
 	motion_fsm(delta)
 	aim_fsm()
-	arm_swing(delta)
+	animations.arm_swing(chest, aim_state, delta)
 	rotate_camera()
 	rotate_player()
-	weapon_sway_and_pose()
-	reset_mouse_motion_event_relative()
+	animations.weapon_sway(right_weapon_pivot, aim_state)
+	weapon_pose()
 	shoot()
 	move_and_slide()
 
@@ -160,6 +145,7 @@ func motion_fsm(delta):
 	match motion_state:
 		IDLE:
 			if not motion_state_entered:
+				current_speed = 0
 				hand_position = Vector3.ZERO
 				standing_collision_shape.set_deferred("disabled", false)
 				crouching_collision_shape.set_deferred("disabled", true)
@@ -170,8 +156,6 @@ func motion_fsm(delta):
 			if head_raycast.is_colliding():
 				switch_motion_state(CROUCH)
 			head.position.y = lerp(head.position.y, STANDING_HEAD_HEIGHT, 0.3)
-			velocity.x = move_toward(velocity.x, 0, normal_deceleraion)
-			velocity.z = move_toward(velocity.z, 0, normal_deceleraion)
 			if direction != Vector3.ZERO:
 				switch_motion_state(RUN)
 			if Input.is_action_pressed("jump"):
@@ -197,23 +181,20 @@ func motion_fsm(delta):
 				switch_motion_state(CROUCH)
 			footsteps()
 			head.position.y = lerp(head.position.y, STANDING_HEAD_HEIGHT, 0.3)
-			velocity.x = move_toward(velocity.x, direction.x * current_speed, acceleraion)
-			velocity.z = move_toward(velocity.z, direction.z * current_speed, acceleraion)
 			if Input.is_action_pressed("sprint"):
 				switch_motion_state(SPRINT)
 		JUMP:
 			if not motion_state_entered:
 				hand_position = Vector3.ZERO
+				current_speed = AIR_SPEED
 				standing_collision_shape.set_deferred("disabled", false)
 				crouching_collision_shape.set_deferred("disabled", true)
-				jump_animation()
+				animations.jump_animation(camera)
 				voice_audio.play()
 				velocity.y = JUMP_FORCE
 				motion_state_entered = true
 			head.position.y = lerp(head.position.y, STANDING_HEAD_HEIGHT, 0.3)
 			velocity.y -= gravity * delta
-			velocity.x = move_toward(velocity.x, direction.x * current_speed, acceleraion)
-			velocity.z = move_toward(velocity.z, direction.z * current_speed, acceleraion)
 			if velocity.y <= 0:
 				switch_motion_state(FALL)
 		FALL:
@@ -221,15 +202,12 @@ func motion_fsm(delta):
 				standing_collision_shape.set_deferred("disabled", false)
 				crouching_collision_shape.set_deferred("disabled", true)
 				hand_position = Vector3.ZERO
+				current_speed = AIR_SPEED
 				motion_state_entered = true
 			head.position.y = lerp(head.position.y, STANDING_HEAD_HEIGHT, 0.3)
 			velocity.y -= gravity * delta
-			if  velocity.y < 0:
-				last_in_air_velocity = velocity.y
 			if Input.is_action_pressed("jump"):
 				switch_motion_state(JUMP)
-			velocity.x = move_toward(velocity.x, direction.x * current_speed, acceleraion)
-			velocity.z = move_toward(velocity.z, direction.z * current_speed, acceleraion)
 			if is_on_floor():
 				switch_motion_state(LAND)
 		SPRINT:
@@ -254,8 +232,6 @@ func motion_fsm(delta):
 				switch_motion_state(CROUCH)
 			footsteps()
 			head.position.y = lerp(head.position.y, STANDING_HEAD_HEIGHT, 0.3)
-			velocity.x = move_toward(velocity.x, direction.x * current_speed, acceleraion)
-			velocity.z = move_toward(velocity.z, direction.z * current_speed, acceleraion)
 		CROUCH:
 			if not motion_state_entered:
 				hand_position = Vector3.ZERO
@@ -270,13 +246,11 @@ func motion_fsm(delta):
 			footsteps()
 			tilt_upper_body()
 			head.position.y = lerp(head.position.y, CROUCHING_HEAD_HEIGHT, 0.3)
-			velocity.x = move_toward(velocity.x, direction.x * current_speed, acceleraion)
-			velocity.z = move_toward(velocity.z, direction.z * current_speed, acceleraion)
 		SLIDE:
 			if not motion_state_entered:
 				slide_start_position = position
 				hand_position = Vector3.ZERO
-				current_speed = CROUCHING_SPEED
+				current_speed = SLIDING_SPEED
 				standing_collision_shape.set_deferred("disabled", true)
 				crouching_collision_shape.set_deferred("disabled", false)
 				motion_state_entered = true
@@ -286,34 +260,30 @@ func motion_fsm(delta):
 				switch_motion_state(FALL)
 			sliding_audio.play()
 			head.position.y = lerp(head.position.y, SLIDING_HEAD_HEIGHT, 0.3)
-			velocity.x = move_toward(velocity.x, 0, slide_deceleraion)
-			velocity.z = move_toward(velocity.z, 0, slide_deceleraion)
 			if slide_start_position.distance_squared_to(position) > pow(slide_max_distance, 2) or is_on_wall():
 				switch_motion_state(IDLE)
 		LAND:
 			if not motion_state_entered:
 				hand_position = Vector3.ZERO
-				land_animation()
-				last_in_air_velocity = 0
+				current_speed = 0
+				animations.land_animation(camera)
 				motion_state_entered = true
 			head.position.y = lerp(head.position.y, STANDING_HEAD_HEIGHT, 0.3)
 			switch_motion_state(IDLE)
+
+	velocity.x = move_toward(velocity.x, direction.x * current_speed, acceleraion)
+	velocity.z = move_toward(velocity.z, direction.z * current_speed, acceleraion)
 
 
 func aim_fsm():
 	match aim_state:
 		HIPFIRE:
 			if not aim_state_entered:
-				sin_amplitude = HIPFIRE_SIN_AMPLITUDE
-				sin_frequency = HIPFIRE_SIN_FREQUENCY
 				mouse_sensitivity = Settings.hipfire_mouse_sensitivity
 				aim_state_entered = true
 			hipfire_mode()
-			get_hand_tilt()
 		ADS:
 			if not aim_state_entered:
-				sin_amplitude = ADS_SIN_AMPLITUDE
-				sin_frequency = ADS_SIN_FREQUENCY
 				mouse_sensitivity = Settings.ads_mouse_sensitivity
 				aim_state_entered = true
 			if motion_state == SPRINT:
@@ -321,19 +291,13 @@ func aim_fsm():
 			ads_mode()
 
 
-func arm_swing(delta):
-	chest.position.y = cos(sin_time * sin_frequency) * sin_amplitude
-	chest.position.x = sin(sin_time * sin_frequency * 0.5) * sin_amplitude
-	sin_time += delta
-
-
 func rotate_camera():
-	head.rotation.x += mouse_motion_event_relative_y * mouse_sensitivity * -1
+	head.rotation.x += mouse_motion_event_relative.y * mouse_sensitivity * -1
 	head.rotation_degrees.x = clamp(head.rotation_degrees.x, -90, 90)
 
 
 func rotate_player():
-	rotation.y += mouse_motion_event_relative_x * mouse_sensitivity * -1
+	rotation.y += mouse_motion_event_relative.x * mouse_sensitivity * -1
 	rotation_degrees.y = wrap(rotation_degrees.y, -180, 180)
 
 
@@ -345,22 +309,10 @@ func hipfire_mode():
 	right_hand.position = lerp(right_hand.position, HIPFIRE_STANCE, 0.5)
 
 
-func get_hand_tilt():
-	if abs(mouse_motion_event_relative_y / viewport_size.y) > hand_tilt_deadzone:
-		hand_tilt.x = sign(mouse_motion_event_relative_y)
-	if abs(mouse_motion_event_relative_x / viewport_size.x) > hand_tilt_deadzone:
-		hand_tilt.y = sign(mouse_motion_event_relative_x)
-		hand_tilt.z = sign(mouse_motion_event_relative_x)
-
-
-func weapon_sway_and_pose():
+func weapon_pose():
 	right_hand.rotation_degrees.x = lerp(right_hand.rotation_degrees.x, hand_position.x, 0.3)
 	right_hand.rotation_degrees.y = lerp(right_hand.rotation_degrees.y, hand_position.y, 0.3)
 	right_hand.rotation_degrees.z = lerp(right_hand.rotation_degrees.z, hand_position.z, 0.3)
-
-	right_weapon_pivot.rotation_degrees.x = lerp(right_weapon_pivot.rotation_degrees.x, sign(hand_tilt.x) * weapon_sway_amount, 0.1)
-	right_weapon_pivot.rotation_degrees.y = lerp(right_weapon_pivot.rotation_degrees.y, sign(-hand_tilt.y) * weapon_sway_amount, 0.1)
-	right_weapon_pivot.rotation_degrees.z = lerp(right_weapon_pivot.rotation_degrees.z, sign(hand_tilt.z) * weapon_sway_amount, 0.1)
 
 
 func _on_weapon_pivot_child_entered_tree(node: Node) -> void:
@@ -378,8 +330,8 @@ func set_visibility(node : Node):
 
 func _input(event):
 	if event is InputEventMouseMotion:
-		mouse_motion_event_relative_x = event.relative.x
-		mouse_motion_event_relative_y = event.relative.y
+		mouse_motion_event_relative.x = event.relative.x
+		mouse_motion_event_relative.y = event.relative.y
 
 	if Input.is_action_just_pressed("reload"):
 		Signals.reload.emit()
@@ -404,8 +356,8 @@ func get_tilt():
 
 
 func reset_mouse_motion_event_relative():
-		mouse_motion_event_relative_x = 0
-		mouse_motion_event_relative_y = 0
+		mouse_motion_event_relative.x = 0
+		mouse_motion_event_relative.y = 0
 
 
 func recoil(recoil_force : Vector2):
@@ -423,37 +375,8 @@ func footsteps():
 		last_step_position = Vector2(position.x, position.z)
 
 
-func head_bob():
-	pass
-
-
-func tilt_head():
-	var head_tilt := 0.0
-	if abs(mouse_motion_event_relative_x / viewport_size.x) > head_tilt_deadzone:
-		head_tilt = -mouse_motion_event_relative_x
-	head.rotation_degrees.z = lerp(head.rotation_degrees.z, HEAD_TILT_DEGREES * sign(head_tilt), 0.1)
-
-
 func tilt_upper_body():
 	upper_body.rotation_degrees.z = lerp(upper_body.rotation_degrees.z, UPPER_BODY_TILT_DEGREES * tilt, 0.3)
-
-
-func jump_animation():
-	var tween = create_tween()
-	tween.tween_property( camera, "position:y", 0.1, 0.1 ).set_trans(Tween.TRANS_LINEAR)
-	tween.parallel().tween_property( camera, "rotation:x", deg_to_rad(-5), 0.1 ).set_trans(Tween.TRANS_LINEAR)
-
-	tween.tween_property( camera, "position:y", 0.0, 0.2 ).set_trans(Tween.TRANS_LINEAR)
-	tween.parallel().tween_property( camera, "rotation:x", 0.0, 0.2 ).set_trans(Tween.TRANS_LINEAR)
-
-
-func land_animation():
-	var tween = create_tween()
-	tween.tween_property( camera, "position:y", 0.1, 0.1 ).set_trans(Tween.TRANS_LINEAR)
-	tween.parallel().tween_property( camera, "rotation:x", deg_to_rad(-5), 0.1 ).set_trans(Tween.TRANS_LINEAR)
-
-	tween.tween_property( camera, "position:y", 0.0, 0.2 ).set_trans(Tween.TRANS_LINEAR)
-	tween.parallel().tween_property( camera, "rotation:x", 0.0, 0.2 ).set_trans(Tween.TRANS_LINEAR)
 
 
 func emit_initial_signals():
